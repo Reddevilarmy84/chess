6abb41e9cae6ec12dde7fe2b6f821acbc72ea9ef
reddevil.army84@gmail.com <reddevil.army84@gmail.com>
2026-09-08 21:58:00 +0300

    ref: simplify class logger

--- main.py
+++ main.py
@@ -9,26 +9,27 @@ class Logger():
     The class describes the
     event logging in the game.
     """
-    
+  
     def __init__(self):
         self._log = list('' for line in range(4))
-        # add the application name and version to the Log sequence
-        self._log.insert(0, "Chess (version: 0.1)")
-        # number of records in log sequence
-        self._counter = 1
 
-    # add new event
+    # add new info record
     def info(self, *args):
         for arg in args:
-            self._log.append(f"{self.counter} {str(arg)}")
-            self._counter += 1
-            if len(self._log > 4):
-                del self._log[0]
+            self._log.append(f" INFO {str(arg)}")
+        self._log = self._log[-4:]
+
+    # add new err record
+        def err(self, *args):
+            for arg in args:
+                self._log.append(f" INFO {str(arg)}")
+            self._log = self._log[-4:]
 
     # display the log
-    def view(self):
+    def display(self):
         for line in reversed(self._log):
             print(line)
+        print()
 
 class Defeated:
     """
